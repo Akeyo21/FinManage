ffd05a1467a70b1dd7a8cb0345293e9b4e3f1953
Akeyo21 <victoria@helloello.com>
2023-04-01 18:43:51 +0300

    Adding the drawer to the main screen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,15 +3,54 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Typography  from '@mui/material/Typography'
-
+import Drawer from '@mui/material/Drawer';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import SwipeableDrawer from '@mui/material/SwipeableDrawer';
+import { useState } from 'react'
+import IconButton from '@mui/material/IconButton'
+import { ICON_HEIGHT, ICON_STYLE, ICON_WIDTH } from '@/utils/constants'
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home(test:any) {
-    const label = { inputProps: { "aria-label": "Switch demo" } };
-    return (
-        <>
+export default function Home({isMobile}:any) {
+  const [openDrawer, setOpenDrawer] = useState(!!isMobile)
+  const handleOpenDrawer =()=>{
+    setOpenDrawer(true)
+  }
 
-            <Typography variant="h1">TEST HEADING</Typography>
-        </>
-    )
+  const handleDrawerClose =()=>{
+    setOpenDrawer(false)
+  }
+  return (
+    <>
+      <IconButton
+        aria-label="open drawer"
+        onClick={handleOpenDrawer}
+        edge="start"
+        sx={{
+          ml: 2,
+          color: "text.primary",
+          ...(openDrawer && { display: 'none' }) }}>
+        <MenuIcon
+          style={ICON_STYLE}
+        />
+      </IconButton>
+
+      <Drawer
+        anchor="left"
+        variant="persistent"
+        onClose={handleDrawerClose}
+        open={openDrawer}>
+        <IconButton onClick={handleDrawerClose}>
+          <ChevronLeftIcon  style={ICON_STYLE}/>
+        </IconButton>
+                Test
+      </Drawer>
+    </>
+
+
+
+
+
+  )
 }
