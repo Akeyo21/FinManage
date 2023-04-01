674042f386a8cf6b980a2ce880ee1631085f4cc0
Akeyo21 <victoria@helloello.com>
2023-04-01 19:01:44 +0300

    Adding icons to the drawer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,20 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import Typography  from '@mui/material/Typography'
+import { useState } from 'react'
+
 import Drawer from '@mui/material/Drawer';
 import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import SwipeableDrawer from '@mui/material/SwipeableDrawer';
-import { useState } from 'react'
 import IconButton from '@mui/material/IconButton'
-import { ICON_HEIGHT, ICON_STYLE, ICON_WIDTH } from '@/utils/constants'
-const inter = Inter({ subsets: ['latin'] })
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import RequestQuoteIcon from '@mui/icons-material/RequestQuote';
+import HourglassFullIcon from '@mui/icons-material/HourglassFull';
+import PaidIcon from '@mui/icons-material/Paid';
+
+import { ICON_STYLE } from '@/utils/constants'
+
 
 export default function Home({isMobile}:any) {
   const [openDrawer, setOpenDrawer] = useState(!!isMobile)
@@ -21,6 +25,21 @@ export default function Home({isMobile}:any) {
   const handleDrawerClose =()=>{
     setOpenDrawer(false)
   }
+
+  const navList = [
+    {
+      title: "Bills",
+      icon: <RequestQuoteIcon/>
+    },
+    {
+      title: "Due Bills",
+      icon: <HourglassFullIcon/>
+    },
+    {
+      title: "Paid Bills",
+      icon: <PaidIcon/>
+    }
+  ]
   return (
     <>
       <IconButton
@@ -41,10 +60,24 @@ export default function Home({isMobile}:any) {
         variant="persistent"
         onClose={handleDrawerClose}
         open={openDrawer}>
-        <IconButton onClick={handleDrawerClose}>
+        <IconButton
+          onClick={handleDrawerClose}
+          sx={{color: "text.primary"}}>
           <ChevronLeftIcon  style={ICON_STYLE}/>
         </IconButton>
-                Test
+        <List>
+          {navList.map(({title, icon}, index)=>(
+            <ListItem disablePadding key={index}>
+              <ListItemButton >
+                <ListItemIcon sx={{color: "text.primary"}}>
+                  {icon}
+                </ListItemIcon>
+                <ListItemText primary={title}/>
+              </ListItemButton>
+            </ListItem>
+          ))}
+
+        </List>
       </Drawer>
     </>
 
